4859f51e901a7c42d237ceb54fcf7e39382ba6ee
Sarthak Kushwaha <kushwahasarthak29@gmail.com>
2026-01-13 11:51:40 +0530

    grpc server | graceful shutdown

--- src/server.cpp
+++ src/server.cpp
@@ -1,13 +1,17 @@
+#include <iostream>
+#include <csignal>
+#include <string>
+#include <thread>
+
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/security/server_credentials.h>
 #include <grpcpp/server.h>
 #include <grpcpp/server_builder.h>
 #include <grpcpp/server_context.h>
 #include <grpcpp/support/status.h>
-#include <string>
+
 #include "api/v1/sat.grpc.pb.h"
 #include "api/v1/sat.pb.h"
-
 #include "server.h"
 
 // Each RPC becomes a virtual method you override
@@ -34,6 +38,23 @@ class PropogationServiceImpl final: public propogation_service::PropogationServi
 	}
 };
 
+std::unique_ptr<grpc::Server> server;
+
+/*
+Shutdown():
+1. Stops accepting new connections
+2. Waits for in-flight RPCs to finish
+3. Safe to call multiple times
+*/
+// https://grpc.github.io/grpc/cpp/classgrpc_1_1_server_interface.html#a6a1d337270116c95f387e0abf01f6c6c
+void signal_handler(int signum) {
+    std::cout << "Interrupt signal (" << signum << ") received. Shutting down server..." << std::endl;
+    if (server) {
+        std::thread shutdown_thread([]{ server->Shutdown(); });
+        shutdown_thread.join();
+    }
+}
+
 void run_server() {
 	std::string server_address("127.0.0.1:50051");
 	PropogationServiceImpl service;
@@ -45,9 +66,15 @@ void run_server() {
 
 	builder.RegisterService(&service);
 
-	std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+	server = builder.BuildAndStart();
 
 	std::cout << "Server listening on " << server_address << '\n';
 
+	// void(*signal(int, void (*)(int)))(int);
+	std::signal(SIGINT, signal_handler);
+	std::signal(SIGTERM, signal_handler);
+
 	server->Wait();
+
+	std::cout << "Server shutdown gracefully\n";
 }
